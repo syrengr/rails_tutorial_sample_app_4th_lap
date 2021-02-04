9c4ef5307b01ed3439dc15ce35e74e144dfffb6d
syrengr <namasaya.sayuri0412@gmail.com>
2021-02-04 15:52:10 +0900

    Implement password updates

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe UserMailer, type: :mailer do
       expect(mail.body.encoded).to match CGI.escape(user.email)
     end
   end
+
+  describe "password_reset" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:mail) { UserMailer.password_reset(user) }
+    before { user.reset_token = User.new_token }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Password reset")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["noreply@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match user.reset_token
+      expect(mail.body.encoded).to match CGI.escape(user.email)
+    end
+  end   
 end
